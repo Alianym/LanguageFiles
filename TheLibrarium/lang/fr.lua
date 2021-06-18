2bb15c4d57d129855c8af1ce52cadbd33f27702d
Alianym <69038163+Alianym@users.noreply.github.com>
2021-06-18 19:26:41 +0800

    Create fr.lua

---
+++ TheLibrarium/lang/fr.lua
@@ -0,0 +1,25 @@
+local stringsFR = {
+   LIBRARIUM_MAIL_ATTACHED = "Jointe",
+   LIBRARIUM_MAIL_SAVE = "Sauvegarder le courrier",
+   LIBRARIUM_CUSTOM_INTERACT_BOOKSHELF = "Étagère",
+}
+
+for id, string in pairs(stringsFR) do
+   ZO_CreateStringId(id, string)
+   SafeAddVersion(id, 2)
+end
+
+LibrMailDateMap.Month = {
+	[01] = "Primétoile",
+	[02] = "Clairciel",
+	[3] = "Semailles",
+	[4] = "Ondepluie",
+	[5] = "Plantaisons",
+	[6] = "Mi-l'an",
+	[7] = "Hautzénith",
+	[8] = "Vifazur",
+	[9] = "Âtrefeu",
+	[10] = "Soufflegivre",
+	[11] = "Sombreciel",
+	[12] = "Soirétoile",
+}
