ae4643f3b082c98bf7a3ffed0ad86d130646af0d
Alianym <69038163+Alianym@users.noreply.github.com>
2021-06-18 19:27:55 +0800

    Create ru.lua

---
+++ TheLibrarium/lang/ru.lua
@@ -0,0 +1,10 @@
+local stringsRU = {
+   LIBRARIUM_MAIL_ATTACHED = "Вложение",
+   LIBRARIUM_MAIL_SAVE = "Сохранить почту",
+   LIBRARIUM_CUSTOM_INTERACT_BOOKSHELF = "Книжная полка",
+}
+
+for id, string in pairs(stringsRU) do
+   ZO_CreateStringId(id, string)
+   SafeAddVersion(id, 2)
+end
