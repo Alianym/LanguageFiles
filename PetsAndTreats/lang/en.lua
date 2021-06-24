81728a0c1ec85d01bb1e56e6671abc08aac3f020
Baertram <pz1@gmx.net>
2021-06-24 12:30:42 +0200

    Changed string
    You should not use "string" as a variable name. It's a standard lua variable

--- PetsAndTreats/lang/en.lua
+++ PetsAndTreats/lang/en.lua
@@ -3,7 +3,7 @@ local stringsEN = {
     PAT_CRITERIA_FORMAT = "You Must First Complete:\n[%s]",
 }
 
-for id, string in pairs(stringsEN) do
-   ZO_CreateStringId(id, string)
+for id, stringVar in pairs(stringsEN) do
+   ZO_CreateStringId(id, stringVar)
    SafeAddVersion(id, 1)
 end
