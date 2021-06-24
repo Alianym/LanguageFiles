08c79166b48b029d4aced8bf8b13cc59358fc080
Baertram <pz1@gmx.net>
2021-06-24 12:31:08 +0200

    Changed string variable

--- CustomItemNotes/lang/en.lua
+++ CustomItemNotes/lang/en.lua
@@ -3,7 +3,7 @@ local stringsEN = {
     CUSTOM_ITEM_NOTES_EDIT_NOTE = GetString(SI_EDIT_NOTE_DIALOG_TITLE),
 }
 
-for id, string in pairs(stringsEN) do
-   ZO_CreateStringId(id, string)
+for id, stringVar in pairs(stringsEN) do
+   ZO_CreateStringId(id, stringVar)
    SafeAddVersion(id, 1)
 end
