94d2d1d9f1cfcf999b7ee7b2c70e6de94fae754c
Baertram <pz1@gmx.net>
2021-06-24 12:33:13 +0200

    Update de.lua

--- PetsAndTreats/lang/de.lua
+++ PetsAndTreats/lang/de.lua
@@ -1,6 +1,6 @@
 local stringsDE = {
     --PAT_GAMEPAD_ACHIEVEMENTS_ITEM_LABEL = GetString(SI_GAMEPAD_SKILLS_TOOLTIP_STATUS)..":",
-    PAT_CRITERIA_FORMAT = "You Must First Complete:\n[%s]",
+    PAT_CRITERIA_FORMAT = "Du musst zunächst dies abschließen:\n[%s]",
 }
 
 for id, stringVar in pairs(stringsDE) do
